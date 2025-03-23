b10f2f56b6de1eaa7b1bb381db0006daf212af35
amatgil <amatgilvinyes@gmail.com>
2025-03-24 00:32:40 +0100

    Fix test

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
-use ternary_statement_macro::{si, Truthyable};
-use ternary_statement_truthability::Truthy;
-
-#[derive(Truthyable)]
-enum Thingy {
-    #[truthy]
-    SoTrue,
-    #[falsey]
-    SoFalse,
-    #[falsey]
-    AlsoFalse,
-    #[truthy]
-    SikeTrueAgain,
-}
+// Rexports
+pub use ternary_statement_macro::{si, Truthyable};
+pub use ternary_statement_truthability::Truthy;
 
 #[test]
 fn test() {
+    #[derive(Truthyable)]
+    enum Thingy {
+        #[truthy]
+        SoTrue,
+        #[falsey]
+        SoFalse,
+        #[falsey]
+        AlsoFalse,
+        #[truthy]
+        SikeTrueAgain,
+    }
     assert_eq!('T', si!(2 > 1 ? 'T' : 'F'));
     assert_eq!('F', si!(2 < 1 ? 'T' : 'F'));
 
